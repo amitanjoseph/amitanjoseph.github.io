eda7bf88cdfbcc7a2763ecec629b240757a87cb4
Amitan Joseph <amitanj@outlook.com>
2025-05-06 23:34:32 +0100

    remove svelte

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import svelte from "@astrojs/svelte";
-
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), mdx()],
+  integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()],
   },
